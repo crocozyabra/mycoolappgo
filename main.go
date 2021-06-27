fcb53b99b79edd59d96e4f578c493282fb14cb92
crocozyabra <68197526+crocozyabra@users.noreply.github.com>
2021-06-27 22:23:52 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
-
-	//"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
+	"github.com/freshman-tech/news-demo-starter-files/news"
 	"github.com/joho/godotenv"
 )
 
@@ -15,23 +17,57 @@ var tpl = template.Must(template.ParseFiles("index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
-	w.Write([]byte("<h1>Hello World!</h1>"))
+}
+
+func searchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Printf("%+v", results)
+	}
 }
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println(("Error loading .env file"))
+		log.Println("Error loading .env file")
 	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
-	mux := http.NewServeMux()
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Env: apiKey must be set")
+	}
 
+	myClient := &http.Client{Timeout: 10 * time.Second}
+	newsapi := news.NewClient(myClient, apiKey, 20)
+
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/search", searchHandler(newsapi))
 	mux.HandleFunc("/", indexHandler)
-	//err := http.ListenAndServe(":"+port, mux)
-	//if err != nil {
-	//	fmt.Println(err)
+	http.ListenAndServe(":"+port, mux)
 }
